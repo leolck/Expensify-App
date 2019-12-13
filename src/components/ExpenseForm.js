5dc488b8d05d382746e66858521b3b5ffbfba3f4
Leo Lai <leo@Leos-MacBook-Air.local>
2019-12-12 22:30:42 -0500

    added cancel button, remove expense confirmation, and some tests on EditExpensePage (not complete)

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 
@@ -54,41 +55,42 @@ export default class ExpenseForm extends React.Component {
     };
     render() {
         return (
-                <form className="form"onSubmit={this.onSubmit}>
+            <form className="form" onSubmit={this.onSubmit}>
                 {this.state.error && <p className="form__error">{this.state.error}</p>}
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        className="text-input"
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        className="text-input"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
-                    <SingleDatePicker
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}
-                    />
-                    <textarea
-                        className='textarea'
-                        value={this.state.note}
-                        placeholder="Add a note for your expense (optional)"
-                        onChange={this.onNoteChange}                   
-                    />
-                    <div>
-                        <button className="button">Save Expense</button>
-                    </div>
-                </form>     
-        )
-    }
-}
+                <input
+                    type="text"
+                    placeholder="Description"
+                    autoFocus
+                    className="text-input"
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Amount"
+                    className="text-input"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+                <textarea
+                    className='textarea'
+                    value={this.state.note}
+                    placeholder="Add a note for your expense (optional)"
+                    onChange={this.onNoteChange}                   
+                />
+                <div className="form-navigation">
+                    <button className="button">Save Expense</button>
+                    <Link to="/" className="button">Cancel</Link>
+                </div>
+            </form> 
+        );
+    };
+};
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -13,10 +13,10 @@ beforeEach(() => {
     };
     wrapper = shallow(
         <EditExpensePage 
-        expense={expenses[0]} 
-        start_edit_expense={start_edit_expense} 
-        history={history} 
-        start_remove_expense={start_remove_expense}
+            expense={expenses[0]} 
+            start_edit_expense={start_edit_expense} 
+            history={history} 
+            start_remove_expense={start_remove_expense}
         />
     );
 });
@@ -31,12 +31,22 @@ test('should handle start_edit_expense', () => {
     expect(history.push).toHaveBeenLastCalledWith('/');
 });
 
-test('should handle start_remove_expense', () => {
+test('should change state after remove expense button clicks', () => {
     wrapper.find('button').simulate('click');
+    expect(wrapper.state('confirmRemove')).toBe(true);
+});
+
+test('should handle start_remove_expense', () => {
+    wrapper.find('RemoveModal').prop('onConfirmRemove');
+    expect(wrapper.state('confirmRemove')).toBe(false);
     expect(start_remove_expense).toHaveBeenLastCalledWith({
         id: expenses[0].id
     });
     expect(history.push).toHaveBeenLastCalledWith('/');
 });
 
+test('should change state to false after user denies remove expense button', () => {
+    wrapper.find('RemoveModal').prop('onDenyRemove');
+    expect(wrapper.state('confirmRemove')).toBe(false);
+});
 
